b51870ae7632b2c63d28d19364f0437cf26d2c95
Leechael Yim <yanleech@gmail.com>
2024-11-17 23:27:04 +0800

    feat: support parse threads when reading a tweet

--- src/actions/twitter/readTweet.ts
+++ src/actions/twitter/readTweet.ts
@@ -11,13 +11,21 @@ export type ReadTweetOptions = {
 export async function readTweet({ screen_name, tweet_id, ...options }: ReadTweetOptions) {
   const { client, xhr$ } = await openPage({ ...(options || {}), url: `https://x.com/${screen_name}/status/${tweet_id}` })
   try {
-  const resp = await firstValueFrom(xhr$.pipe(matchedUrl('TweetDetail')))
-  const body = await resp.json()
-  const matched1 = (body?.data?.threaded_conversation_with_injections_v2?.instructions || []).filter((i: any) => i.type === 'TimelineAddEntries')
-  const matched2 = (matched1[0] || { entries: [] }).entries.filter((i: any) => i.entryId.startsWith('tweet-'))
-  if (matched2.length) {
-    return extractTweet(matched2[0]?.content?.itemContent?.tweet_results?.result)
-  }
+    const resp = await firstValueFrom(xhr$.pipe(matchedUrl('TweetDetail')))
+    const body = await resp.json()
+    const matched1 = (body?.data?.threaded_conversation_with_injections_v2?.instructions || []).filter((i: any) => i.type === 'TimelineAddEntries')
+    const entries = (matched1[0] || { entries: [] }).entries || []
+    const matched2 = entries.filter((i: any) => i.entryId.startsWith('tweet-'))
+    if (matched2.length) {
+      const tweet = extractTweet(matched2[0]?.content?.itemContent?.tweet_results?.result)
+      // Try find out self-thread
+      const selfThread = entries.find((i: any) => i?.content?.displayType === 'VerticalConversation' && i?.content.items[0]?.item?.itemContent?.tweetDisplayType === 'SelfThread')
+      const threads = (selfThread?.content?.items || []).map((i: any) => extractTweet(i.item?.itemContent?.tweet_results?.result))
+      tweet.threads = threads
+      return tweet
+    }
+  } catch (err) {
+    console.log('err', err)
   } finally {
     await client.close()
   }
