1846dbd41b6341d83e8c94d6b24ad47aa9d31ba9
Leechael Yim <yanleech@gmail.com>
2024-11-17 23:26:40 +0800

    setup logger & api design

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
 import { Hono } from 'hono'
+import { logger } from 'hono/logger'
 import { serve } from '@hono/node-server'
 
 import { readHomeTimeline, readUserTimeline, readMentions, readTweet, postTweet } from '@/actions/twitter'
 
 const app = new Hono()
+
+app.use(logger())
 
 app.get('/home_timeline', async (ctx) => {
   const tweets = await readHomeTimeline()
@@ -27,10 +30,19 @@ app.get('/user/:screen_name/:tweet_id', async (ctx) => {
   return ctx.json(tweet)
 })
 
+app.get('/user/:screen_name/status/:tweet_id', async (ctx) => {
+  const { screen_name, tweet_id } = ctx.req.param()
+  const tweet = await readTweet({ screen_name, tweet_id })
+  return ctx.json(tweet)
+})
+
 app.post('/tweets', async (ctx) => {
   const { text } = await ctx.req.json()
   await postTweet({ text: text as string })
   return ctx.json({ "success": true })
 })
 
-serve(app)
+serve({
+  fetch: app.fetch,
+  port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
+})
