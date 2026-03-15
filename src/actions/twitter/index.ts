5e11147382a90d8f0330b08a9c42d0da21fedf3f
Leechael Yim <yanleech@gmail.com>
2026-03-15 10:52:21 +0800

    feat(twitter): export search and user timeline types

--- src/actions/twitter/index.ts
+++ src/actions/twitter/index.ts
@@ -1,7 +1,8 @@
 export { postTweet } from '@/actions/twitter/postTweet'
 
 export { readHomeTimeline } from '@/actions/twitter/readHomeTimeline'
-export { readUserTimeline } from '@/actions/twitter/readUserTimeline'
+export { readUserTimeline, type UserTimelineTab, type ReadUserTimelineOptions } from '@/actions/twitter/readUserTimeline'
 export { readMentions } from '@/actions/twitter/readMentions'
 export { readTweet, SessionExpiredError } from '@/actions/twitter/readTweet'
+export { search, buildSearchQuery, type SearchOptions } from '@/actions/twitter/search'
 
